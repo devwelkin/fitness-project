05d7f36845dc22bdbdc1a9166f700e05406a8bbb
devwelkin <112391727+devwelkin@users.noreply.github.com>
2025-09-26 13:45:11 +0300

    feat: Create Activity Service
    - Add Activity model and ActivityType enum for activity tracking

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,15 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    CYCLING,
+    WALKING,
+    SWIMMING,
+    WEIGHTING_TRAINING,
+    YOGA,
+    HIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+
+}
