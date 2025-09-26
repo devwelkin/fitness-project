9bcd3a5586719fcda6647d546ac79b31aa3ec9f4
devwelkin <112391727+devwelkin@users.noreply.github.com>
2025-09-26 10:49:46 +0300

    feat: Update PostgreSQL configuration and user model for improved data handling

--- userservice/src/main/java/com/fitness/userservice/model/User.java
+++ userservice/src/main/java/com/fitness/userservice/model/User.java
@@ -6,14 +6,16 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "users")
 @Data
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private Long id;
+    private UUID id;
 
     @Column(nullable = false, unique = true)
     private String email;
